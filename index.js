2e14970f49d73a469e3c470ace6883cc0c69c2b1
Maxlaseave <89351211+Maxlaseave@users.noreply.github.com>
2024-12-19 15:42:41 +0800

    https

--- index.js
+++ index.js
@@ -2,16 +2,19 @@ const express = require('express');
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
 const winston = require('winston');
+const https = require('https');
+const path = require('path');
+const fs = require('fs');
 
 require('dotenv').config();
 
-// Import route modules
+
 const authRoutes = require('./src/routes/auth.routes');
 const crmRoutes = require('./src/routes/crm.routes');
 const inventoryRoutes = require('./src/routes/inventory.routes');
 const supportRoutes = require('./src/routes/support.routes');
 
-// Import middleware
+
 const { authenticateToken } = require('./src/middleware/auth.middleware');
 const { requestLogger } = require('./src/middleware/logging.middleware');
 
@@ -45,9 +48,17 @@ app.use((err, req, res, next) => {
   });
 });
 
-// Server startup
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+// Server startup https
+const sslServer = https.createServer(
+  {
+    key: fs.readFileSync(path.join(__dirname, 'certificates', 'key.pem')),
+    cert: fs.readFileSync(path.join(__dirname, 'certificates', 'cert.pem')),
+  },
+  app
+)
+
+sslServer.listen(PORT, ()=> {
+  console.log(`Service on Secure Server running on port ${PORT}`);
 });
 
 module.exports = app;
